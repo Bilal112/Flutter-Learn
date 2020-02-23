ecf7cc39a9d359da1616f1843da0b1e1114ee889
bilal112 <bilalirfan000@gmail.com>
2020-02-23 16:53:18 +0500

    button add

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-            title: Text(
-          "Hi From Bilal ",
-          style: TextStyle(
-              backgroundColor: Colors.deepOrange, color: Colors.deepPurple),
-        )),
-        body: Center(
-          child: Text(
-            "Hello Flutter I am Learning Flutter",
-            textAlign: TextAlign.left,
-            textDirection: TextDirection.rtl,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,
-            style: TextStyle(
-              fontSize: 40.0,
-              color: Colors.red,
-              textBaseline: TextBaseline.alphabetic,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+        title: 'Hello World',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
         ),
-      ),
-    );
+        home: Scaffold(
+          appBar: AppBar(
+              title: Text(
+            "Hi From Bilal ",
+            style: TextStyle(
+                backgroundColor: Colors.deepOrange, color: Colors.deepPurple),
+          )),
+          body: Center(
+            child: MaterialButton(
+                onPressed: () {},
+                splashColor: Colors.green,
+                color: Colors.red,
+                child: Text(
+                  "data a loading",
+                  
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+
+                  )
+                  ,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14)
+                )
+                ),
+          ),
+        ));
   }
 }
