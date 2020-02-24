61a6957ade31c9c94e5bd24f000b36e64bf26ab4
bilal112 <bilalirfan000@gmail.com>
2020-02-24 18:35:53 +0500

    flutter statefull componet

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,69 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Hello World',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-              title: Text(
-            "Hi From Bilal ",
-            style: TextStyle(
-                backgroundColor: Colors.deepOrange, color: Colors.deepPurple),
-          )),
-          body: Center(
-            child:IconButton(onPressed: (){},
-            icon: Icon(
-              Icons.add_a_photo
+      theme:  ThemeData(
+        primaryColor: Colors.cyan
+      ),
+      title: "My App",
+      home: Myhome(),
+      
+    );
+  }
+}
+class Myhome extends StatefulWidget {
+
+
+
+  @override
+  _MyhomeState createState() => _MyhomeState();
+}
+
+class _MyhomeState extends State<Myhome> {
+   int count = 0;
+
+
+void updateCount (){
+  setState(() {
+    count += 1;
+  });
+
+
+}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bilal")
+      ),
+      body: Center(
+
+        child : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text("$count",
+            style: TextStyle(fontSize: 30.0,color: Colors.cyan),
             ),
-            iconSize: 50.0,
-            splashColor: Colors.green,
-            tooltip: "Add a Photo"
-            ,)
-          ),
-        ));
+            RaisedButton(onPressed: updateCount,
+            color: Colors.cyan,
+          
+            splashColor: Colors.cyan,
+
+            child: Text("On Click",style:TextStyle(fontSize: 20.5,color: Colors.white,
+       )))
+          ],
+        )
+      ),
+
+    );
   }
 }
